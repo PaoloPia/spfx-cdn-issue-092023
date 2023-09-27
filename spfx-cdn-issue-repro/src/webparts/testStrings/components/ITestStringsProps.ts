01b3d85592a99ed509a12531e0e91b0dec81a25c
Paolo Pialorsi <paolo@pialorsi.com>
2023-09-27 17:14:38 +0200

    Initial commit

---
+++ spfx-cdn-issue-repro/src/webparts/testStrings/components/ITestStringsProps.ts
@@ -0,0 +1,7 @@
+export interface ITestStringsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ spfx-cdn-issue-repro/src/webparts/testStrings/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITestStringsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TestStringsWebPartStrings' {
+  const strings: ITestStringsWebPartStrings;
+  export = strings;
+}
